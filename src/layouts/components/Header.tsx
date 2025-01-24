db515a6da77ef61ccff86a5dd09cecb170f7611e
Shakhzod Namazbaev <shakxzod.namazbaev.9700@gmail.com>
2025-01-24 18:07:43 +0500

    style: upgrade tailwindcss v3 to v4

--- src/layouts/components/Header.tsx
+++ src/layouts/components/Header.tsx
@@ -4,7 +4,7 @@ import { SidebarTrigger } from '@/shared/components/ui/sidebar';
 
 export function Header() {
   return (
-    <header className="sticky top-0 shadow flex justify-between items-center bg-white z-10 px-6 py-4 2xl:py-5 w-full">
+    <header className="sticky top-0 shadow-sm flex justify-between items-center bg-white z-10 px-6 py-4 2xl:py-5 w-full">
       <SidebarTrigger className="-ml-1" />
       <UserDropdown />
     </header>
