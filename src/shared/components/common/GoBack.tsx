15e5ba191af96783ea207897be57549703571585
namazbaev <shakxzod.namazbaev.9700@gmail.com>
2025-01-26 22:03:53 +0500

    feat: create new go back component

---
+++ src/shared/components/common/GoBack.tsx
@@ -0,0 +1,37 @@
+import { useNavigate } from 'react-router-dom';
+import { ChevronLeft } from 'lucide-react';
+import React, { memo } from 'react';
+
+interface GoBackProps {
+  title: string;
+  fallbackPath?: string;
+}
+
+const GoBack: React.FC<GoBackProps> = memo(({ title, fallbackPath = '/' }) => {
+  const navigate = useNavigate();
+
+  const handleGoBack = () => {
+    if (window.history.state && window.history.length > 2) {
+      navigate(-1);
+    } else {
+      navigate(fallbackPath);
+    }
+  };
+
+  return (
+    <div className="flex items-center gap-2">
+      <button
+        aria-label="back"
+        onClick={handleGoBack}
+        className="flex items-center"
+      >
+        <ChevronLeft className="w-5 h-5" />
+      </button>
+      <h1 className="text-xl font-semibold truncate text-neutral-850">
+        {title}
+      </h1>
+    </div>
+  );
+});
+
+export default GoBack;
