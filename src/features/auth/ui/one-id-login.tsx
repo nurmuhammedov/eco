8b9bdf968e8d210314403472ff4b59f928e359a3
Shakhzod Namazbaev <shakxzod.namazbaev.9700@gmail.com>
2025-05-06 12:40:26 +0500

    refactor: implement one id keys

--- src/features/auth/ui/one-id-login.tsx
+++ src/features/auth/ui/one-id-login.tsx
@@ -8,7 +8,7 @@ export default function LoginForm() {
     <div className="w-1/2 flex flex-col items-center justify-center">
       <h3 className="mb-4 font-medium text-2xl leading-6 w-1/4 text-center">Axborot tizimiga kirish</h3>
       <a
-        href={`https://sso.egov.uz/sso/oauth/Authorization.do?response_type=one_code&client_id=ysn_uztn_uz&redirect_uri=https://ekotizim.cirns.uz/auth/login&scope=myportal&state=testState`}
+        href={`https://sso.egov.uz/sso/oauth/Authorization.do?response_type=one_code&client_id=ekotizim_cirns_uz&client_secret=rfxcTm2qC69r0Ar29Gjw286Z&redirect_uri=https://ekotizim.cirns.uz/auth/login&scope=myportal&state=testState`}
         className="bg-neutral-200 px-9 3xl:px-12 rounded-2xl inline-block"
       >
         <Icon name="id-gov" className="w-32 h-20 3xl:w-36 3xl:h-24" />
