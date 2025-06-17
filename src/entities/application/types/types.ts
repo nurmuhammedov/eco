5f9a8af73dcfebf1c0e8456c05102d564e52a9a5
nurmuhammedov <topcoder.n@gmail.com>
2025-06-17 21:42:47 +0500

    fix some

--- src/entities/application/types/types.ts
+++ src/entities/application/types/types.ts
@@ -8,6 +8,15 @@ export enum ApplicationStatus {
   REJECTED = 'REJECTED',
 }
 
+export enum ApplicationStatusForInspector {
+  ALL = 'ALL',
+  IN_PROCESS = 'IN_PROCESS',
+  IN_AGREEMENT = 'IN_AGREEMENT',
+  IN_APPROVAL = 'IN_APPROVAL',
+  COMPLETED = 'COMPLETED',
+  REJECTED = 'REJECTED',
+}
+
 export interface ApplicationFilters {
   name: string;
   search: string;
