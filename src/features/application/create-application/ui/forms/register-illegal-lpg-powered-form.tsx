cf8cb28595b53238739c52cb34fe94cf64d3abed
topcoder <topcoder.n@gmail.com>
2026-05-13 12:35:35 +0500

    feat: some fix

--- src/features/application/create-application/ui/forms/register-illegal-lpg-powered-form.tsx
+++ src/features/application/create-application/ui/forms/register-illegal-lpg-powered-form.tsx
@@ -498,7 +498,7 @@ export default ({ onSubmit, isPending = false }: RegisterIllegalLpgPoweredFormPr
                 <FormItem className={'mb-2'}>
                   <div className="flex flex-col justify-between gap-2 sm:flex-row sm:items-center">
                     <FormLabel required className="w-full sm:max-w-1/2 2xl:max-w-3/7">
-                      Ruxsatnoma fayli
+                      Ruxsatnoma
                     </FormLabel>
                     <FormControl>
                       <InputFile form={form} name={field.name} accept={[FileTypes.PDF]} />
